1f36f207e4818c5fd0398889145560977b107923
ivic <534747943@qq.com>
2020-06-02 04:33:09 +0000

    未开始活动的情况，每天提醒一次 确保监控是正常运行的

--- check.py
+++ check.py
@@ -6,17 +6,21 @@ import time
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+import time
 
-
-def alarm_email():
+def alarm_email(flag):
     from_addr = '发件邮箱@163.com'
     password = '邮箱系统开启IMAP/SMTP服务后提供的密码'
     to_addr = '收件邮箱@qq.com'
     smtp_server = 'smtp.163.com'
-    msg = MIMEText('正文', 'plain', 'utf-8')
+    if flag==1:
+        msg = MIMEText('暑期返校开始', 'plain', 'utf-8')
+        msg['Subject'] = Header('apple活动已经上线')
+    else:
+        msg = MIMEText('暑期返校尚未开始', 'plain', 'utf-8')
+        msg['Subject'] = Header('apple活动尚未上线')
     msg['From'] = Header(from_addr)
     msg['To'] = Header(to_addr)
-    msg['Subject'] = Header('标题')
     server = smtplib.SMTP_SSL()
     server.connect(smtp_server, 465)
     server.login(from_addr, password)
@@ -32,14 +36,16 @@ def check():
         if res.status_code != 200:
             return 0
         text = res.text
-        # text += "Beats耳机"
+#        text += "Beats耳机"
         if text.find("耳机") != -1 or text.find("Beats") != -1:
             print("活动开始啦!")
-            alarm_email()
-            return 1
+            alarm_email(1)
         else:
             print("text length=", len(text))
-        return 0
+            localtime = time.localtime(time.time())
+            if localtime.tm_hour==0:
+                alarm_email(0)
+        return 1
     except Exception as e:
         print("error!", e)
         return 0
